7fbaac528632974df382ed6c9d805b255dcae9da
mrfirki <mrfikri94@gmail.com>
2017-03-17 12:17:06 +0800

    create validation and shorter methods

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ApplicationRecord
+
+	validates :long_url, :presence => true , :format => {:with => /((?:https?\:\/\/|www\.)(?:[-a-z0-9]+\.)*[-a-z0-9]+.*)/i }#URI::regexp(["http", "https"]) #%w(http https)
+
+	def shorten
+		 self.short_url = SecureRandom.hex(3)
+	end	
 end
